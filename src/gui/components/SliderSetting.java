d961fac23c1f548489a04c6f6738a46675897bd9
Eric Tsang <surplus.et@gmail.com>
2015-11-11 02:28:05 -0800

    added more settings controls
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- src/gui/components/SliderSetting.java
+++ src/gui/components/SliderSetting.java
@@ -1,32 +1,30 @@
-package gui;
+package gui.components;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 
-import java.util.NoSuchElementException;
 import java.util.Optional;
 
-/**
- * Created by Eric Tsang on 11/8/2015.
- */
 public class SliderSetting
 {
-    public Optional<Label> label;
-    public Optional<NumberTextField> numberTextField;
-    public Optional<Slider> slider;
+    private final boolean allowDecimalNumbers;
+    private Optional<Label> label;
+    private Optional<NumberTextField> numberTextField;
+    private Optional<Slider> slider;
+
+    public SliderSetting(boolean allowDecimalNumbers)
+    {
+        this.allowDecimalNumbers = allowDecimalNumbers;
+        label = Optional.empty();
+        numberTextField = Optional.empty();
+        slider = Optional.empty();
+    }
 
     public DoubleProperty valueProperty()
     {
         return getSlider().valueProperty();
-    }
-
-    public SliderSetting()
-    {
-        label = Optional.empty();
-        numberTextField = Optional.empty();
-        slider = Optional.empty();
     }
 
     public Label getLabel()
@@ -46,6 +44,9 @@ public class SliderSetting
                 numberTextField.get().textProperty().addListener((value,oldValue,newValue) ->
                     {
                         getSlider().setValue(Double.parseDouble(newValue));
+                        getNumberTextField().setText(allowDecimalNumbers
+                            ? Double.toString(getSlider().getValue())
+                            : Integer.toString((int) Math.round(getSlider().getValue())));
                     });
                 return numberTextField.get();
             });
