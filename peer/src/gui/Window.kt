6d31a59d1be9c7678450f0fc8d6e40a96bcd8c4b
Eric Tsang <surplus.et@gmail.com>
2015-11-30 18:54:06 -0800

    switcing computers
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- peer/src/gui/Window.kt
+++ peer/src/gui/Window.kt
@@ -1,6 +1,9 @@
 package gui
 
+import ProtocolPeer.ClientSocket
+import ProtocolPeer.Connection
 import javafx.application.Application
+import javafx.beans.InvalidationListener
 import javafx.scene.Scene
 import javafx.scene.control.ScrollPane
 import javafx.scene.layout.BorderPane
@@ -9,11 +12,15 @@ import javafx.stage.Stage
 class Window:Application()
 {
     private val WINDOW_TITLE = "Window Title"
+
     private val WINDOW_WIDTH = 1000
     private val WINDOW_HEIGHT = 480
 
     private val connectionsPane = ConnectionsPane()
     private val serverPane = ServerPane()
+    private val clientSocketObserver = ClientSocketObserver()
+
+    private var clientSocket:ClientSocket? = null
 
     override fun start(primaryStage:Stage)
     {
@@ -24,6 +31,25 @@ class Window:Application()
         primaryStage.scene = Scene(ContentPane(),WINDOW_WIDTH.toDouble(),WINDOW_HEIGHT.toDouble())
         primaryStage.scene.stylesheets.add(CSS.FILE_PATH)
         primaryStage.show()
+
+        serverPane.port.addListener(InvalidationListener
+        {
+            clientSocket = ClientSocket(serverPane.port.value,clientSocketObserver)
+            clientSocket!!.setListening(serverPane.listening.value)
+        })
+        serverPane.listening.addListener(InvalidationListener
+        {
+            clientSocket?.setListening(serverPane.listening.value)
+        })
+    }
+
+    private inner class ClientSocketObserver:ClientSocket.Observer
+    {
+        override fun onAccept(newConnection:Connection?)
+        {
+            // todo: implement
+            throw UnsupportedOperationException()
+        }
     }
 
     private inner class ContentPane:BorderPane()
